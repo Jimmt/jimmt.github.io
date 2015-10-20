4705d7455ac824e0b4c6375fd960d3739798f27c
Jimmt <quantum.physics98@gmail.com>
2015-10-19 18:21:24 -0700

    favicon, google play links, clear-face font

--- projectdisplay.js
+++ projectdisplay.js
@@ -5,7 +5,7 @@ var projects = [
 	"name": "Puck Slide",
 	"ref": "puckslide",
 	"description": "Slide a hockey puck across snow and mud with precision in this addicting arcade game. Integrated with Google Play Game Services for high scores and achievements.",
-	"platforms": ["google_play"],
+	"platforms": [{"name": "google_play", "package" : "com.jumpbuttonstudio.puckslide.android"}],
 	"tools": ["libgdx"],
 	"width": 480,
 	"height": 280,
@@ -14,7 +14,7 @@ var projects = [
 	"name": "Totem Stack",
 	"ref": "totems",
 	"description": "Stack randomly falling totems and build your stack higher and higher until you reach the skies. Diverse gameplay with powerups, random events, and a dynamic background, as well as integration with Google Play Game Services for achievements and high scores.",
-	"platforms": ["google_play"],
+	"platforms": [{"name": "google_play", "package" : "com.jbs.totemgame.android"}],
 	"tools": ["libgdx"],
 	"width": 200,
 	"height": 320,
@@ -23,7 +23,7 @@ var projects = [
 	"name": "Smite Training",
 	"ref": "smite",
 	"description": "<b>1000+ downloads\n</b> Practice your smiting skill from League of Legends in realistic scenarios. Randomized smite damage on both dragon and baron, with multiplayer on the same phone supported. Integrated with Google Play Game Services for high scores.",
-	"platforms": ["google_play"],
+	"platforms": [{"name": "google_play", "package" : "com.jimmt.smitepractice.android"}],
 	"tools": ["libgdx"],
 	"width": 520,
 	"height": 315,
@@ -32,7 +32,7 @@ var projects = [
 	"name": "Hologram Clock",
 	"ref": "hologram",
 	"description": "A holographic clock for those with a four-sided plastic hologram apparatus (easily made from tutorials on youtube). Features many different vibrant particle effects with color options.",
-	"platforms": ["google_play"],
+	"platforms": [{"name": "google_play", "package" : "com.jimmt.HologramClock.android"}],
 	"tools": ["libgdx"],
 	"width": 375,
 	"height": 215,
@@ -41,7 +41,7 @@ var projects = [
 	"name": "Infection: FBLA 2015",
 	"ref": "fbla",
 	"description": "<b>Made for FBLA Computer Game & Simulation 2015\n</b> Pilot your spaceship and shoot your enemies as you navigate through mutliple levels and experience unique boss fights. Upgrade your ship, or change its visual appearance to customize your experience.",
-	"platforms": ["google_play"],
+	"platforms": [],
 	"tools": ["libgdx"],
 	"width": 550,
 	"height": 300,
@@ -50,7 +50,7 @@ var projects = [
 	"name": "Rainbow Hippie 2",
 	"ref": "fbla",
 	"description": "Experience the vibrant action of shooting rainbows and dodging obstacles, fighting dangeous monsters along the way. Visit the skies, ocean, and underground in your journey through the game!",
-	"platforms": ["google_play"],
+	"platforms": [],
 	"tools": ["libgdx"],
 	"width": 550,
 	"height": 300,
@@ -121,7 +121,7 @@ function updatePanel(project, td){
 		var descrip = document.createElement("p");
 		descrip.id = "descriptionText";
 		descrip.className = "projectPanelText";
-		var descripText = document.createTextNode(projects[i].description);
+		var descripText = document.createTextNode(projects.description);
 		descrip.appendChild(descripText);
 		td.appendChild(descrip);
 	}
@@ -131,5 +131,24 @@ function updatePanel(project, td){
 	img.height = project.height;
 	var descrip = document.getElementById("descriptionText");
 	descrip.innerHTML = project.description;
+
+	var display = document.getElementById("display");
+	var platformLinks = display.getElementsByClassName("platformLink");
+	for(var i = 0; i < platformLinks.length; i++){
+		display.removeChild(platformLinks[i]);
+	}
+
+	for(var i = 0; i < project.platforms.length; i++){
+		if(project.platforms[i].name == "google_play"){
+			var playLink = document.createElement("a");
+			playLink.className = "platformLink";
+			playLink.href = "https://play.google.com/store/apps/details?id=" + project.platforms[i].package;
+			var playImage = document.createElement("img");
+			playImage.src = "https://developer.android.com/images/brand/en_app_rgb_wo_60.png";
+			playImage.alt = "Android app on Google Play";
+			playLink.appendChild(playImage);
+			display.appendChild(playLink);
+		}
+	}
 
 }
